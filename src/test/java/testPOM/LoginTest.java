50ee2e4d47d7ede1b1f155112daade0a15069a75
ZKiskinova <zlatina.kiskinova@seznam.cz>
2023-05-21 11:02:53 +0200

    @FindBy(css="") 	@FindBy(css="") 	private WebElement webElement; 	private WebElement webElement;

--- src/test/java/testPOM/LoginTest.java
+++ src/test/java/testPOM/LoginTest.java
@@ -2,7 +2,6 @@ package testPOM;
 
 import base.TestUtil;
 import org.testng.annotations.Test;
-import pages.DocuWarePage;
 import pages.LoginPage;
 
 public class LoginTest extends TestUtil {
@@ -10,7 +9,7 @@ public class LoginTest extends TestUtil {
     @Test
     public void successfulLogin(){
         LoginPage loginPage = new LoginPage(driver);
-        DocuWarePage Page = loginPage.login("Златина Колева","77777777");
+        loginPage.login("Златина Колева","77777777");
     }
 
 }
